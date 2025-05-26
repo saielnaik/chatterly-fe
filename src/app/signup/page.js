f7418836142e4ab66a76259835c3f7c782ed5674
saiel naik <74955042+saielnaik@users.noreply.github.com>
2025-05-26 19:41:22 +0530

    Initial

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { signup } from '../../api/auth';
 import { useRouter } from 'next/navigation';
 
